e6dec1c61b6c138027489d2572dd90cfeda942af
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2019-02-27 16:04:07 +0100

    Created root library source file
    Added prototype Player and Hand structs
    Update TOML file
    Library types now declared inside modules

---
+++ lib/card.rs
@@ -0,0 +1,98 @@
+// Copyright (C) 2019 Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation (version 3).
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+pub mod card {
+	use std::vec;
+	use rand::Rng;
+
+	#[no_mangle]
+	#[repr(C)]
+	pub struct Deck {
+		cards: Vec<i32>,
+		deck_count: usize,
+		card_index: usize
+	}
+
+	#[no_mangle]
+	pub static DIAMONDS: i32 = 0b0001_0000;
+
+	#[no_mangle]
+	pub static HEARTS: i32   = 0b0010_0000;
+
+	#[no_mangle]
+	pub static CLUBS: i32    = 0b0100_0000;
+
+	#[no_mangle]
+	pub static SPADES: i32   = 0b1000_0000;
+
+	#[no_mangle]
+	pub static SUIT: i32     = 0b1111_0000;
+
+	#[no_mangle]
+	pub static VALUE: i32    = 0b0000_1111;
+
+	impl Deck {
+		pub fn new(deck_count: usize) -> Deck {
+			let mut cards: Vec<i32> = Vec::with_capacity(52 * deck_count);
+			for _ in 0..=deck_count {
+				for suit in vec![DIAMONDS, HEARTS, CLUBS, SPADES] {
+					for card in 1..=13 {
+						cards.push(card | suit);
+					}
+				}
+			}
+			Deck { cards, deck_count, card_index: 0 }
+		}
+
+		pub fn shuffle(&mut self) {
+			for i in (1..=self.deck_count * 52).rev() {
+				let idx: usize = rand::thread_rng().gen_range(0, i as usize);
+				let curr = i as usize;
+				let tmp = self.cards[curr];
+				self.cards[curr] = self.cards[idx];
+				self.cards[idx] = tmp;
+			}
+			self.card_index = 0;
+		}
+
+		pub fn next_card(&mut self) -> i32 {
+			let card = self.cards[self.card_index];
+			self.card_index += 1;
+			card
+		}
+	}
+
+	#[no_mangle]
+	pub extern "C" fn deck_new(deck_count: usize) -> *mut Deck {
+		Box::into_raw(Box::new(Deck::new(deck_count)))
+	}
+
+	#[no_mangle]
+	pub extern "C" fn deck_shuffle(ptr: *mut Deck) {
+		let deck = unsafe {
+			assert!(!ptr.is_null());
+			&mut *ptr
+		};
+		deck.shuffle();
+	}
+
+	#[no_mangle]
+	pub extern "C" fn deck_getNextCard(ptr: *mut Deck) -> i32 {
+		let deck = unsafe {
+			assert!(!ptr.is_null());
+			&mut *ptr
+		};
+		deck.next_card()
+	}
+}
---
+++ lib/player.rs
@@ -0,0 +1,119 @@
+// Copyright (C) 2019 Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation (version 3).
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+pub mod player {
+	use std::vec;
+	use crate::card::card::*;
+
+	#[no_mangle]
+	pub struct Player {
+		isDealer: bool,
+		hands: Vec<Hand>,
+		balance: i32,
+		standing: i32
+	}
+
+	#[no_mangle]
+	pub struct Hand {
+		cards: Vec<i32>,
+		wager: i32
+	}
+
+	impl Player {
+		pub fn new(isDealer: bool, balance: i32) -> Player {
+			Player {
+				isDealer, hands: Vec::with_capacity(2), balance, standing: 0
+			}
+		}
+
+		pub fn bet(&mut self, wager: i32, deck: &mut Deck) {
+			self.hands.push(Hand::new(wager, deck));
+		}
+
+		pub fn split(&mut self, deck: &mut Deck) -> bool {
+			for mut hand in &mut self.hands {
+				if let Some(newhand) = hand.split(deck) {
+					self.hands.push(newhand);
+					return true;
+				}
+			}
+			false
+		}
+
+		pub fn hit(&mut self, deck: &mut Deck) -> bool {
+			self.hands[0].hit(deck)
+		}
+
+		pub fn has_busted(&self) -> bool {
+			for hand in &self.hands {
+				if hand.busted() {
+					return true;
+				}
+			}
+			false
+		}
+	}
+
+	impl Hand {
+		pub fn new(wager: i32, deck: &mut Deck) -> Hand {
+			let mut hand = Hand { cards: Vec::with_capacity(11), wager };
+			for _ in 0..2 {
+				hand.cards.push(deck.next_card())
+			}
+			hand
+		}
+
+		pub fn split(&mut self, deck: &mut Deck) -> Option<Hand> {
+			if self.cards.len() == 2 {
+				if (self.cards[0] & VALUE) == (self.cards[1] & VALUE) {
+					let card = self.cards[1];
+					self.cards[1] = deck.next_card();
+					return Some(Hand {
+						cards: vec![card, deck.next_card()], wager: self.wager
+					});
+				}
+			}
+			None
+		}
+
+		pub fn hit(&mut self, deck: &mut Deck) -> bool {
+			self.cards.push(deck.next_card());
+			self.busted()
+		}
+
+		pub fn busted(&self) -> bool {
+			let mut total = 0;
+			for card in &self.cards {
+				total += card & VALUE;
+			}
+			total > 21
+		}
+	}
+
+	#[no_mangle]
+	pub extern "C" fn player_new(isDealer: bool, balance: i32) -> *mut Player {
+		Box::into_raw(Box::new(Player::new(isDealer, balance)))
+	}
+
+	#[no_mangle]
+	pub extern "C" fn player_isDealer(player: *mut Player) -> bool {
+		false
+	}
+
+	#[no_mangle]
+	pub extern "C" fn player_getBalance(player: *const Player) -> i32 { 0 }
+
+	#[no_mangle]
+	pub extern "C" fn player_getStanding(player: *const Player) -> i32 { 0 }
+}
